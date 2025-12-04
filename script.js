124881582946196b235ecff706e62e16d7958990
alessiodalberto002 <aledalberto002.com>
2025-12-04 18:36:09 +0100

    inserimento sconti

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const km = prompt("quanti km vuoi percorrere?");
-const price = 0.21 * km;
 const eta = prompt("inserisci la tua età");
 
+const price = 0.21 * km;
+const scontoMinori = price * (20/100);
+const scontoAnzini = price * (40/100)
+const prezzoFinaleMinori = price - scontoMinori; 
+const prezzoFinaleAnziani = price - scontoAnzini;
+
+if(eta < 18 || eta > 65){
+    console.log(prezzoFinaleMinori);
+    console.log(prezzoFinaleAnziani);
+}
